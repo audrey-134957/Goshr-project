e26f6ad4da0dbd9f192c4eb8b556166ef0ec99ab
audrey-134957 <milard.audrey@gmail.com>
2020-09-22 00:30:37 +0200

    nettoyage code

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="{{asset('./images/logo/logo.png')}}" class="logo" alt="Goshr Logo">
+<h1>Goshr</h1>
 @else
 {{ $slot }}
 @endif
