21c0fb8fd7a0cbfa42e6e3137f08f8ca4c74c2f9
audrey-134957 <milard.audrey@gmail.com>
2020-09-22 23:04:25 +0200

    nettoyage code

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,15 +2,13 @@
 
         <footer class="footer">
             <nav class="footer__navigation">
-                <div>
-                    <span style="display: block;margin-bottom:2rem;">Contactez-nous</span>
+                <div class="footer__contact-box">
+                    <span>Contactez-nous</span>
                     <a class="footer__mail-address" href="mailto:help.goshr@gmail.com">help.goshr@gmail.com</a>
                 </div>
                 <ul class="footer__list-items footer__list-items--infos">
                     <li class="footer__item"><a class="footer__link" href="#">mentions légales</a></li>
                     <li class="footer__item footer__item--is-cgu"><a class="footer__link" href="#">c.g.u</a></li>
-                    <!-- <li clas><a href="mailto:help.goshr@gmail.com">help.goshr@gmail.com</a></li> -->
-                    <!-- <li class="footer__item footer__item--is-contact"><a class="footer__link" href="{{--route('contact.create')--}}">contactez-nous</a></li> -->
                 </ul>
 
                 <ul class="footer__list-items footer__list-items--networks">
